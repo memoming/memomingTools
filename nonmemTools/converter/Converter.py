3aa3f97c3a7fd4102fa54aa209581ebe7419799f
WOOJAE LIM <memoming.dev@gmail.com>
2019-12-05 15:54:50 +0900

    Update Conterver

--- nonmemTools/converter/Converter.py
+++ nonmemTools/converter/Converter.py
@@ -19,11 +19,42 @@ class Converter  :
     
     def convertCSV(self) :
         csvFileList = self.config.getStateValue("csvFileList")
+        pathRltDir  = os.path.join(self.config.getStateValue("dirPath"),"convertedCSV")
+        os.makedirs(pathRltDir, exist_ok=True)
+        
         for eachCSV in csvFileList :
-            pathEachCSV = os.path.join(self.config.getStateValue("dirPath"),eachCSV)
-            wholeLine = None
+            timeLine    = None
+            wholeData   = list()
+            pathEachCSV = os.path.join(self.config.getStateValue("dirPath"),"csvForms",eachCSV)
+            pathRltCSV  = os.path.join(pathRltDir,"converted_"+eachCSV)
+
             with open(pathEachCSV,"r") as csvStream :
-                wholeLine = 
+                reader = csv.reader(csvStream)
+                for index, each in enumerate(reader,start=0) :
+                    if index==0 : 
+                        timeLine = each[1:]
+                        continue
+                    else : wholeData.append(each[2:])
+
+                
+            with open(pathRltCSV,"w",encoding="utf-8") as csvStream :
+                writer = csv.writer(csvStream)
+                writer.writerow(["ID","TIME","DV","MDV"])
+
+                for numID,eachAgent in enumerate(wholeData,start=1) :
+                    for i, eachData in enumerate(eachAgent,start=0) :
+                        if eachData == "BQL" : eachData = "."
+                        if eachData == "-" : continue
+                        eachLine = [numID]              # ID
+                        eachLine.append(timeLine[i])    # TIME
+                        eachLine.append(eachData)       # DV
+                        if i == 0 : eachLine.append(1)  # MDV
+                        else : eachLine.append(0)
+                        writer.writerow(eachLine)
+
+
+
+        
 
 
 
